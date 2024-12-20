82f418e6d68b7cb34a05e29d2b184c644b86b5ed
GenshinMinecraft <gm@highp.ing>
2024-12-20 21:40:26 +0800

    docs(unlock): 增加 utils.rs 与 mod.rs 中的注释

--- src/unlock_test/mod.rs
+++ src/unlock_test/mod.rs
@@ -63,40 +63,54 @@ pub async fn check_all() {
 
 impl Display for UnlockResult {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        // 创建一个标准输出流，并设置颜色选择为 Always
         let mut stdout = StandardStream::stdout(ColorChoice::Always);
 
+        // 设置前景色为白色
         stdout
             .set_color(ColorSpec::new().set_fg(Some(Color::White)))
             .unwrap();
+
+        // 根据 available 字段的值来决定输出内容
         let result = if self.available {
+            // 设置前景色为绿色
             stdout
                 .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
                 .unwrap();
+            // 根据 region 字段是否存在来决定输出内容
             match &self.region {
                 None => {
+                    // 输出服务名称
                     write!(f, "{}", self.service_name)
                 }
                 Some(region) => {
+                    // 输出服务名称和地区
                     write!(f, "{} ({})", self.service_name, region)
                 }
             }
         } else {
+            // 设置前景色为红色
             stdout
                 .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
                 .unwrap();
+            // 根据 error 字段是否存在来决定输出内容
             match &self.error {
                 None => {
+                    // 输出服务名称
                     write!(f, "{}", self.service_name)
                 }
                 Some(error) => {
+                    // 输出服务名称和错误信息
                     write!(f, "{}: {}", self.service_name, error)
                 }
             }
         };
 
+        // 恢复前景色为白色
         stdout
             .set_color(ColorSpec::new().set_fg(Some(Color::White)))
             .unwrap();
+        // 返回格式化结果
         result
     }
 }
--- src/unlock_test/utils.rs
+++ src/unlock_test/utils.rs
@@ -3,33 +3,64 @@ use reqwest::header::HeaderMap;
 use reqwest::Client;
 use std::time::Duration;
 
+/// 从字符串的开头和结尾修剪指定数量的字符
+///
+/// # 参数
+/// * `s` - 要修剪的字符串
+/// * `leading` - 要从字符串开头修剪的字符数
+/// * `trailing` - 要从字符串结尾修剪的字符数
+///
+/// # 返回值
+/// * 修剪后的字符串切片
 pub fn trim_string(s: &str, leading: usize, trailing: usize) -> &str {
+    // 计算修剪后的字符串的起始索引
     let start = leading;
+    // 计算修剪后的字符串的结束索引，使用 saturating_sub 方法避免索引越界
     let end = s.len().saturating_sub(trailing);
+    // 返回修剪后的字符串切片
     &s[start..end]
 }
 
+/// 创建一个 reqwest 客户端
+///
+/// # 参数
+/// * `service_name` - 服务名称
+/// * `ua` - 用户代理字符串，可选
+/// * `enable_cookies` - 是否启用 cookie 存储，默认值为 false
+/// * `timeout` - 请求超时时间，可选
+///
+/// # 返回值
+/// * `Ok(Client)` - 如果客户端创建成功
+/// * `Err(UnlockResult)` - 如果客户端创建失败
 pub async fn create_reqwest_client(
     service_name: String,
     ua: Option<&str>,
     enable_cookies: bool,
     timeout: Option<u64>,
 ) -> Result<Client, UnlockResult> {
+    // 创建一个 reqwest 客户端构建器
     let mut builder = Client::builder();
+    // 如果用户代理字符串存在，则设置用户代理
     if ua.is_some() {
         builder = builder.user_agent(ua.unwrap());
     }
+    // 如果启用 cookie 存储，则设置 cookie 存储
     if enable_cookies {
         builder = builder.cookie_store(true);
     }
+    // 如果超时时间存在，则设置超时时间
     if timeout.is_some() {
         builder = builder.timeout(Duration::from_secs(timeout.unwrap()));
     } else {
+        // 如果超时时间不存在，则设置默认超时时间为 15 秒
         builder = builder.timeout(Duration::from_secs(15));
     }
 
+    // 构建客户端
     match builder.build() {
+        // 如果客户端构建成功，则返回 Ok(Client)
         Ok(client) => Ok(client),
+        // 如果客户端构建失败，则返回 Err(UnlockResult)
         Err(_) => Err(UnlockResult {
             service_name,
             available: false,
@@ -44,12 +75,24 @@ pub const UA_BROWSER: &str = r#"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleW
 // Android
 pub const UA_BROWSER2: &str = r#"Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36"#;
 
+/// 将响应解析为 HTML 字符串
+///
+/// # 参数
+/// * `service_name` - 服务名称
+/// * `result` - 要解析的响应
+///
+/// # 返回值
+/// * `Ok(String)` - 如果解析成功，返回 HTML 字符串
+/// * `Err(UnlockResult)` - 如果解析失败，返回错误信息
 pub async fn parse_response_to_html(
     service_name: String,
     result: reqwest::Response,
 ) -> Result<String, UnlockResult> {
+    // 尝试将响应解析为文本
     match result.text().await {
+        // 如果解析成功，返回 HTML 字符串
         Ok(html) => Ok(html),
+        // 如果解析失败，返回错误信息
         Err(_) => Err(UnlockResult {
             service_name,
             available: false,
@@ -59,21 +102,38 @@ pub async fn parse_response_to_html(
     }
 }
 
+/// 发送 HTTP GET 请求到指定的 URL
+///
+/// # 参数
+/// * `service_name` - 服务名称
+/// * `client` - 用于发送请求的 reqwest 客户端
+/// * `url` - 要请求的 URL
+/// * `header_map` - 可选的请求头映射
+///
+/// # 返回值
+/// * `Ok(reqwest::Response)` - 如果请求成功，返回响应
+/// * `Err(UnlockResult)` - 如果请求失败，返回错误信息
 pub async fn get_url(
     service_name: String,
     client: &Client,
     url: &str,
     header_map: Option<HeaderMap>,
 ) -> Result<reqwest::Response, UnlockResult> {
+    // 创建一个 GET 请求
     let mut request = client.get(url);
+    // 如果存在 header_map，则将其添加到请求中
     if header_map.is_some() {
         request = request.headers(header_map.unwrap());
     }
 
+    // 发送请求并等待响应
     let result = request.send().await;
 
+    // 匹配响应结果
     match result {
+        // 如果响应成功，返回响应
         Ok(result) => Ok(result),
+        // 如果响应失败，返回错误信息
         Err(_) => Err(UnlockResult {
             service_name,
             available: false,
