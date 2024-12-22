77b75daa76b0be05e5a077adaf6ece24a56c8b47
moohr <mhr@mhr.hk>
2024-12-22 21:08:13 +0800

    fix: memory calculation

--- src/info/disk.rs
+++ src/info/disk.rs
@@ -11,17 +11,33 @@ pub fn get_disk() -> Vec<String> {
             }
         }
 
-        if disk.total_space() > 1_073_741_824 {
+        /* For some weird reason, memory seems to be calculated entirely differently than storage in sysinfo. 
+           Disk is *lobytes while memory is *bibytes
+           IDK why but whatever
+        */
+
+        if disk.total_space() > 1_000_000_000 {
+            #[cfg(target_os = "linux")]
+            {   
+                /*  why tf is there a random 1GB partition for some linux systems? Only god knows
+                    probably just a swap or boot partition
+                    either way, no way that's going to be used
+                    gonna skip it until I can figure out a better way
+                */
+                if disk.total_space() < 2_000_000_000 {
+                    continue;
+                }
+            }
             diskinfo.push(format!(
                 "{}/{} GB",
-                (disk.total_space() - disk.available_space()) / 1_073_741_824,
-                disk.total_space() / 1_073_741_824
+                (disk.total_space() - disk.available_space()) / 1_000_000_000,
+                disk.total_space() / 1_000_000_000
             ));
         } else {
             diskinfo.push(format!(
                 "{}/{} MB",
-                (disk.total_space() - disk.available_space()) / 1_048_576,
-                disk.total_space() / 1_048_576
+                (disk.total_space() - disk.available_space()) / 1_000_000,
+                disk.total_space() / 1_000_000
             ));
         }
     }
--- src/info/mem.rs
+++ src/info/mem.rs
@@ -1,9 +1,9 @@
 use sysinfo::System;
 pub fn get_mem(s: &System) -> String {
     let memory = s.total_memory();
-    if memory > 1_048_576 {
-        format!("{} GB", memory / 1_073_741_824)
+    if memory > 1_024_000 {
+        format!("{} GB", memory / 1_024_000_000)
     } else {
-        format!("{} MB", memory / 1_048_576)
+        format!("{} MB", memory / 1_024_000)
     }
 }
