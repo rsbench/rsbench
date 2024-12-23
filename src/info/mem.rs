aefa3a7e0f46d8f4d15e2eb074142a00be5f6d1a
moohr <mhr@mhr.hk>
2024-12-24 02:44:39 +0800

    fix: 0GB RAM output on low RAM systems

--- src/info/mem.rs
+++ src/info/mem.rs
@@ -1,7 +1,7 @@
 use sysinfo::System;
 pub fn get_mem(s: &System) -> String {
     let memory = s.total_memory();
-    if memory > 1_024_000 {
+    if memory > 1_024_000_000 {
         format!("{} GB", memory / 1_024_000_000)
     } else {
         format!("{} MB", memory / 1_024_000)
